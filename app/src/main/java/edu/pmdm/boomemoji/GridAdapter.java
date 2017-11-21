c92e2e8de8199ddba4f8983d676783f31cf85f88
aquelo <rmenmar264@gmail.com>
2017-11-21 07:49:55 +0100

    Correcciones

---
+++ app/src/main/java/edu/pmdm/boomemoji/GridAdapter.java
@@ -0,0 +1,51 @@
+package edu.pmdm.boomemoji;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+import java.util.ArrayList;
+
+/**
+ * Created by rafa on 20/11/17.
+ */
+
+public class GridAdapter extends BaseAdapter {
+    private Context context;
+    private ArrayList<Personaje> tablero;
+    public GridAdapter(Context context, ArrayList<Personaje> tablero){
+        this.context = context;
+        this.tablero = tablero;
+    }
+    @Override
+    public int getCount() {
+        return tablero.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+//        AppCompatImageButton button=null;
+//        View gview = new View(this.context);
+//        if(view==null){
+//            gview = ((Activity)this.context).getLayoutInflater().inflate(R.layout.ui_item,null);
+//            button = (AppCompatImageButton)gview.findViewById(R.id.imageButton);
+//            button.setScaleType(ImageView.ScaleType.CENTER_CROP);
+//            button.setBackgroundResource(tablero.get(i).getImagen());
+//            button.setTag(tablero.get(i).getTag());
+//        }else{
+//            return view;
+//        }
+        return view;
+    }
+}
